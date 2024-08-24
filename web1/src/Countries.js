e4fd2c1cdd1ad890d21fc003dc71c3cabb264ebd
naiasiaz <naiasitiazzahra27@gmail.com>
2024-08-24 15:29:46 +0700

    sidebar reusable

---
+++ web1/src/Countries.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Countries() {
+  return (
+    <div>
+      <h3>Countries List</h3>
+      <ul>
+        <li>Country 1</li>
+        <li>Country 2</li>
+        <li>Country 3</li>
+        {/* Tambahkan daftar negara lainnya */}
+      </ul>
+    </div>
+  );
+}
+
+export default Countries;
