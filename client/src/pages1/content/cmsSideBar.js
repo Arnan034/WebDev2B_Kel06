23c48c64bb804de36e39209ddaee94fa86b433e0
naiasiaz <naiasitiazzahra27@gmail.com>
2024-11-03 17:21:58 +0700

    update cmsSidebar

--- client/src/pages1/content/cmsSideBar.js
+++ client/src/pages1/content/cmsSideBar.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 const CmsSidebar = () => {
     const [role, setRole] = useState(''); // State untuk menyimpan role dari localStorage
     const navigate = useNavigate(); // Digunakan untuk navigasi
+    const location = useLocation();
 
-    // Inisialisasi nilai dari localStorage
     useEffect(() => {
-        const storedRole = 'admin';//localStorage.getItem('role');
+        const storedRole = 'admin'; //localStorage.getItem('role');
         if (storedRole) {
             setRole(storedRole);
         } else {
@@ -15,12 +15,14 @@ const CmsSidebar = () => {
         }
     }, [navigate]);
 
+    const isDramasActive = location.pathname === '/cms/validate' || location.pathname === '/cms';
+
     return (
         <div className="sidebar-left-cms bg-selective-yellow-color" id="sidebar-left">
             <div className="container-fluid">
                 <ul className="side-list">
                     {/* Semua pengguna dapat mengakses halaman Movie */}
-                    <li className="side">
+                    <li className={`side ${location.pathname === '/' ? 'active' : ''}`}>
                         <i className="fas fa-users fa-lg"></i>
                         <Link to="/">
                             <span>Movie</span>
@@ -29,22 +31,22 @@ const CmsSidebar = () => {
 
                     {/* Semua pengguna dapat mengakses halaman Input New Drama */}
                     <li className="side">
-                        <div className="side-link">
+                        <div className={`side-link ${isDramasActive ? 'active' : ''}`}>
                             <i className="fas fa-film fa-lg"></i>
                             <span>Dramas</span>
                             <i className="fas fa-caret-down ms-2 fa-lg"></i>
                         </div>
                         <ul className="side flex-column ms-3" id="dramasSubmenu">
                             {role === 'admin' && (
-                                <li className="side">
-                                    <i className="fas fa-circle-check side-link"></i>
+                                <li className={`side ${location.pathname === '/cms/validate' ? 'active' : ''}`}>
+                                    <i className="fas fa-circle-check"></i>
                                     <Link to="/cms/validate" className="side-link">
                                         <span className="fs-6">Validate</span>
                                     </Link>
                                 </li>
                             )}
-                            <li className="side">
-                                <i className="fas fa-pen-to-square side-link"></i>
+                            <li className={`side ${location.pathname === '/cms' ? 'active' : ''}`}>
+                                <i className="fas fa-pen-to-square"></i>
                                 <Link to="/cms" className="side-link">
                                     <span className="fs-6">Input New Drama</span>
                                 </Link>
@@ -55,49 +57,51 @@ const CmsSidebar = () => {
                     {/* Hanya admin yang bisa mengakses menu berikut */}
                     {role === 'admin' ? (
                         <>
-                            <li className="side">
+                            <li className={`side ${location.pathname === '/cms/countries' ? 'active' : ''}`}>
                                 <i className="fas fa-globe fa-lg"></i>
                                 <Link to="/cms/countries">
                                     <span>Countries</span>
                                 </Link>
                             </li>
-                            <li className="side">
+                            <li className={`side ${location.pathname === '/cms/awards' ? 'active' : ''}`}>
                                 <i className="fas fa-award fa-lg"></i>
                                 <Link to="/cms/awards">
                                     <span>Awards</span>
                                 </Link>
                             </li>
-                            <li className="side">
+                            <li className={`side ${location.pathname === '/cms/genres' ? 'active' : ''}`}>
                                 <i className="fas fa-masks-theater fa-lg"></i>
                                 <Link to="/cms/genres">
                                     <span>Genres</span>
                                 </Link>
                             </li>
-                            <li className="side">
-                                <i className="fas fa-user fa-lg side-link"></i>
+                            <li className={`side ${location.pathname === '/cms/actors' ? 'active' : ''}`}>
+                                <i className="fas fa-user fa-lg"></i>
                                 <Link to="/cms/actors" className="side-link">
                                     <span>Actors</span>
                                 </Link>
                             </li>
-                            <li className="side">
+                            <li className={`side ${location.pathname === '/cms/comments' ? 'active' : ''}`}>
                                 <i className="fas fa-comments fa-lg"></i>
                                 <Link to="/cms/comments">
                                     <span>Comments</span>
                                 </Link>
                             </li>
-                            <li className="side">
+                            <li className={`side ${location.pathname === '/cms/users' ? 'active' : ''}`}>
                                 <i className="fas fa-users fa-lg"></i>
                                 <Link to="/cms/users">
                                     <span>Users</span>
                                 </Link>
                             </li>
                         </>
-                    ) : (<li className="side">
-                        <i className="fa-solid fa-bookmark"></i>
-                        <Link to="/cms/bookmark">
-                            <span>Bookmark</span>
-                        </Link>
-                    </li>)}
+                    ) : (
+                        <li className={`side ${location.pathname === '/cms/bookmark' ? 'active' : ''}`}>
+                            <i className="fa-solid fa-bookmark"></i>
+                            <Link to="/cms/bookmark">
+                                <span>Bookmark</span>
+                            </Link>
+                        </li>
+                    )}
                 </ul>
             </div>
         </div>
