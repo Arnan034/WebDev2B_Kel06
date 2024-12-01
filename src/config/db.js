3dc0de240fef924ed2257276a7c79a88645ba6af
Arnan <arnanda.prasatya.tif422@polban.ac.id>
2024-12-01 11:59:22 +0700

    rollback err1

--- src/config/db.js
+++ src/config/db.js
@@ -4,17 +4,18 @@ const dotenv = require('dotenv');
 // Memuat file .env
 dotenv.config();
 
-if (!process.env.DB_URL) {
+if (!process.env.DATABASE_URL) {
     console.error('DATABASE_URL is not set in .env file!');
     process.exit(1);
 }
+
 
 let instance = null;
 
 const createPool = () => {
     if (!instance) {
         instance = new Pool({
-            connectionString: process.env.DB_URL,
+            connectionString: process.env.DATABASE_URL,
             ssl: {
                 rejectUnauthorized: false,
             },
